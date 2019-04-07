bac88cd64008b3eec1e6ec733c28c8bf5f29e0ba
Snuchycovich <emiliano.caslep@gmail.com>
2019-04-07 15:57:42 +0200

    add vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
-  render: h => h(App),
+    //template: '<App/>',
+    //components: { App },
+    router,
+    render: h => h(App),
 }).$mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="row col">
+            <h1>homepage</h1>
+        </div>
+
+        <div class="row col">
+            <p>This is the home page of Cover Football application</p>
+        </div>
+
+        <!--<autocomplete></autocomplete>-->
+        <login-form></login-form>
+    </div>
+</template>
+
+<script>
+    //import autocomplete from '../components/form/fields/autocomplete'
+    import loginForm from '../components/security/login'
+
+    export default {
+        name: 'home',
+        components: {
+            //autocomplete,
+            loginForm
+        }
+    }
+</script>
